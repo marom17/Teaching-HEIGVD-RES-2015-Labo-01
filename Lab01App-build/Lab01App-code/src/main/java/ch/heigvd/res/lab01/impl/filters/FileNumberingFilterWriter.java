ff9d0b26c1832e4f19f83f7735edda7e5e21ec9e
marom17 <romain.flac@gmail.com>
2015-03-15 10:09:50 +0100

    Writer string, buf changé; modification et passage a Utils.getNexLine()

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
@@ -1,5 +1,6 @@
 package ch.heigvd.res.lab01.impl.filters;
 
+import ch.heigvd.res.lab01.impl.Utils;
 import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
@@ -19,8 +20,7 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
   private int nbLine=1;
-  private String previous="";
-  private boolean first=true;
+  private boolean previousR=false;
 
   public FileNumberingFilterWriter(Writer out) {
     super(out);
@@ -29,59 +29,21 @@ public class FileNumberingFilterWriter extends FilterWriter {
   @Override
   public void write(String str, int off, int len) throws IOException {
       
-      String sep="";
       String buf="";
-      String temp=str.substring(off, len-off);
-      if(!previous.equals("")){
-          if(temp.contains("\r\n")||previous.contains("\r\n")){
-              sep="\r\n";
-          }
-          else if(temp.contains("\n")||previous.contains("\n")){
-              sep="\n";
-          }
-          else if(temp.contains("\r")||previous.contains("\r")){
-              sep="\r";
-          }
+      String temp=str.substring(off, off+len);
+      
+      String[] split = Utils.getNextLine(temp);
+      
+      if(nbLine==1){
+          out.write((nbLine++)+"\t");
       }
-      else{
-          if(temp.contains("\r\n")){
-              sep="\r\n";
-          }
-          else if(temp.contains("\n")){
-              sep="\n";
-          }
-          else if(temp.contains("\r")){
-              sep="\r";
-          }
+      
+      while(!split[0].isEmpty()){
+          buf+=split[0]+(nbLine++)+"\t";
+          split = Utils.getNextLine(split[1]);
       }
-      String[] split=temp.split(sep);
-      if(previous.equals("")){
-          buf+=nbLine+"\t";
-          nbLine++;
-      }
-      /*
-      else if(previous.endsWith(sep)){
-          buf+=nbLine+"\t";
-          nbLine++;
-      }*/
-      buf+=split[0];
-      if(split.length>1){
-          buf+=sep;
-      }
-      for(int i=1;i<split.length;i++){
-          buf+=nbLine+"\t"+split[i];
-          nbLine++;
-          if(i!=split.length-1){
-              buf+=sep;
-          }
-      }
-      if(temp.endsWith(sep)){
-          buf+=sep+nbLine+"\t";
-          nbLine++;
-      }
-      previous="";
-      previous=previous.concat(buf);
-      super.write(buf, off, buf.length());
+      buf+=split[1];
+      out.write(buf);
       
   }
 
@@ -92,22 +54,30 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   @Override
   public void write(int c) throws IOException {
-          if(first){
-              first=false;
-              super.write(Integer.toString(nbLine));
+      String buf="";
+          if(nbLine==1){
+              buf=Integer.toString(nbLine);
               nbLine++;
           }
-          if(c==(int)'\n'){
+          
+          if(c==(int)'\n'&&previousR==true){
               
-              super.write((char)c);
-              super.write(Integer.toString(nbLine));
+              buf+=((char)c)+nbLine+"\t";
               nbLine++;
+              previousR=false;
             }
           else{
-              super.write((char)c);
+              buf+=(char)c;
           }
-          super.flush();
-    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
+          if(c==(int)'\r'){
+              previousR=true;
+              buf+=(char)c;
+          }
+          else{
+              previousR=false;
+          }
+          out.write(buf);
+          out.flush();
   }
 
 }
